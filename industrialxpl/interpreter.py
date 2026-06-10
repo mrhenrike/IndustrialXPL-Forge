e4cb24ef78a4231a141af54e1c477fec6dc4b6ca
mrhenrike <henrique.santos@uniaogeek.com.br>
2026-06-10 00:57:16 -0300

    fix: license string em vez de license-file; setuptools>=70; bump 1.0.24

--- industrialxpl/interpreter.py
+++ industrialxpl/interpreter.py
@@ -36,7 +36,7 @@ from industrialxpl.core.exploit.utils import (
     module_required, MODULES_DIR,
 )
 
-VERSION = "1.0.23"
+VERSION = "1.0.24"
 
 _BANNER = r"""
  ___           _           _        _       ___  ______  _          _____
